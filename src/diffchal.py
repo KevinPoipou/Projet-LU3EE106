f90b2f84e44e45452ece589e4b76e3b7ec9191fe
emdle <118718631+emdle@users.noreply.github.com>
2022-11-21 09:18:02 +0100

    Add files via upload

---
+++ src/diffchal.py
@@ -0,0 +1,31 @@
+"""
+Programme comprenant les fonctions utiles au projet
+@version: 1.0
+"""
+
+from dataclasses import dataclass
+
+
+"""
+Dataclass pour les materiaux
+"""
+@dataclass
+class materiau :
+    """
+    Constantes
+    """
+    K : float # Conductivite thermique [ W / (K * m) ]
+    C : float # Capacite calorifique [ J / (kg * K) ]
+    rho : float # Masse volumique [ kg / (m ^ 3) ]
+    
+    """
+    Calcul et retourne la valeur de alfa
+    @param1: self
+    """
+    def get_alfa(self) :
+        return self.K / (self.rho * self.C)
+        
+        
+Aluminium = get_datas()
+def get_datas():
+    ## Renvoie dataclass Materiau
